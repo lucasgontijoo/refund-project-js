d8f478b2259eb2048435ff7c17229ba1337635e9
Lucas Gontijo da Cunha <lucasgontijoo470@gmail.com>
2025-05-20 10:33:07 -0300

    formatação do input de valor e início do tratamento de submit do formulário

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// Seleciona os elementos do formulário
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Captura o evento de input para formatar o valor
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "") // Verifica quais caracteres não são números e substitui por nada
+    
+    // Transforma o valor em centavos (exemplo: 150/100 = 1.5 equivalente a R$1,50)
+    value = Number(value)/100
+
+    amount.value = formatCurrencyBRL(value) // Passa o valor da função formatadora para o valor do input, dessa forma, não aceitando números
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    // Retorna o valor formatado
+    return value
+}
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+})
