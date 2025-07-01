01168f4f8a540260155105e8641b305ee41626a3
Lucas Gontijo da Cunha <lucasgontijoo470@gmail.com>
2025-07-01 10:37:39 -0300

    criando objeto de uma nova despesa

--- scripts.js
+++ scripts.js
@@ -1,25 +1,44 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
-    // Verifica não-dígito em todo conteúdo e substitui por ""
-    let value = amount.value.replace(/\D/g, "") 
+    // Obtém o valor atual do input e remove os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, "")
 
-    // Transforma o valor em centavos
+    // Transformar o valor em centatos (exemplo: 150/100 = 1.5 é equivalente a R$ 1,50)
     value = Number(value) / 100
-    
-    // Passa o valor formatado para o valor do input
+
+    // Atualiza o valor do input
     amount.value = formatCurrencyBRL(value)
 }
 
 function formatCurrencyBRL(value) {
-    // Formata o valor no padrão BRL
+    // Formata o valor no padrão BRL (Real Brasileiro)
     value = value.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL"
     })
 
-    // Retorna o valor formatado
+    // Retorna o valor formatado 
     return value
+}
+
+// Captura o evento de submit do formulário para obter os valores 
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
 }
